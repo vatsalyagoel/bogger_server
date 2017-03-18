dd4ef9f0f0815ed20d16b2dc0f0afccc0717c5a8
Stefanie Yap <slyap11@student.monash.edu>
2017-03-18 21:07:24 +1100

    removed this

--- server.js
+++ server.js
@@ -4,55 +4,59 @@ let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
-var fuelConsumption = 0.0;
-var tonnage = 0.0;
-var throughput = 0.0;
-var teamTonnage = 1000.0;
-
-
-var setFuelConsumption = function () {
-    var min = 16.0;
-    var max = 30.0;
-    this.fuelConsumption = Math.random().nextFloat() * (max - min) + min;
-};
-
-var setTonnage = function () {
-    var min = 13.0;
-    var max = 24.0;
-    this.tonnage = Math.random().nextFloat() * (max - min) + min;
-};
-
-var setThroughput = function () {
-    var min = 350.0;
-    var max = 500.0;
-    this.throughput = Math.random().nextFloat() * (max - min) + min;
-};
-
-var teamTonnage = function () {
-    this.teamTonnage += this.tonnage;
-};
-
 io.on('connection', (socket) => {
+
+  var fuelConsumption = 0.0;
+  var tonnage = 0.0;
+  var throughput = 0.0;
+  var teamTonnage = 1000.0;
+
+
+  var setFuelConsumption = function () {
+      var min = 16.0;
+      var max = 30.0;
+      fuelConsumption = Math.random() * (max - min) + min;
+  };
+
+  var setTonnage = function () {
+      var min = 13.0;
+      var max = 24.0;
+      tonnage = Math.random() * (max - min) + min;
+  };
+
+  var setThroughput = function () {
+      var min = 350.0;
+      var max = 500.0;
+      throughput = Math.random() * (max - min) + min;
+  };
+
+  var setTeamTonnage = function () {
+      teamTonnage += tonnage;
+  };
+
   console.log('user connected');
   
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
   
-  setTimeout(setTeamTonnage, 10000);
+  setTimeout(function() {
+    setTeamTonnage();
+  }, 10000);
 
   setTimeout(function () {
-    this.setFuelConsumption();
-    this.setTonnage();
-    this.setThroughput();
+    setFuelConsumption();
+    setTonnage();
+    setThroughput();
 
     io.emit('data', {
-        fuelConsumption: this.fuelConsumption, 
-        tonnage: this.tonnage,
-        throughput: this.throughput,
-        teamThroughput: this.teamThroughput,
+        fuelConsumption: fuelConsumption, 
+        tonnage: tonnage,
+        throughput: throughput,
+        teamTonnage: teamTonnage,
         currentTime: new Date().getTime()
-    });    
+    });
+    console.log(teamTonnage);    
   }, 500);
     
 });
